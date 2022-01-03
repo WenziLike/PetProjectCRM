ac556cc61d0fe13551d44ed798da1e3e5f0bbeac
WenziLike <SlavaMyraxin@icloud.com>
2022-01-03 11:14:10 +0100

    init project Spring Boot + Angular

---
+++ backendCRM/src/test/java/com/crm/backendcrm/BackendCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.backendcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
