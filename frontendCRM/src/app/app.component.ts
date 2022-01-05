b5015253bb4a373796659c73bb45462e8a0c4f03
WenziLike <SlavaMyraxin@icloud.com>
2022-01-05 23:58:03 +0100

    modified project

--- frontendCRM/src/app/app.component.ts
+++ frontendCRM/src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {EmployeeService} from "./employee.service";
 import {Employee} from "./employee";
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   public employees: Employee[] | undefined;
@@ -30,4 +31,47 @@ export class AppComponent implements OnInit {
       }
     );
   }
+
+  public onAddEmployee(addForm: NgForm): void {
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response)
+        this.getEmployees()
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      },
+    )
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    // container.appendChild(button);
+    button.click();
+    if (container === null) {
+      alert('oops');
+    } else {
+      container.appendChild(button);
+      button.click();
+      // since you've done the nullable check
+      // TS won't complain from this point on
+
+    }
+  }
 }
+
